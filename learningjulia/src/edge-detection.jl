d59348ae26e583751d145e31f8e726a89eca2a48
Madhav Kandukuri <madhav165@gmail.com>
2022-08-23 21:51:40 +0530

    corrected scaling

--- learningjulia/src/edge-detection.jl
+++ learningjulia/src/edge-detection.jl
@@ -45,13 +45,13 @@ function sujoy(img; four_connectivity=false)
     return grad
 end
 
-img_path = "learningjulia/data/elevation3.png"
+img_path = "learningjulia/data/elevation4.png"
 img = load(img_path)
 img_edge = sujoy(img, four_connectivity=true)
 img_edge₀₁ = binarize(img_edge, Otsu()) # or use other binarization methods provided in ImageBinarization
 
 # mosaicview(img, img_edge, img_edge₀₁; nrow = 3)
-save("learningjulia/data/elevation_edges3.png", img_edge₀₁)
+save("learningjulia/data/elevation_edges4.png", img_edge₀₁)
 
 img_matrix = Float32.(img_edge₀₁)
 one_tpl = Tuple.(findall(x -> !iszero(x), img_matrix))
@@ -76,8 +76,13 @@ x_count = combine(groupby(one_df, :y), nrow => :n)
 maximum(x_count[:,:n])
 x_axis = x_count[findall(x -> x==maximum(x_count[:,:n]),x_count[:,:n]),:y]
 
-two_df = one_df[one_df.x .> y_axis, :]
-two_df = two_df[two_df.y .> x_axis, :]
+two_df = filter(row -> row.x > maximum(y_axis) 
+            && row.y > maximum(x_axis), one_df)
+
+plot(two_df[:,:x], two_df[:,:y], seriestype = :scatter)
+
+# two_df = one_df[one_df.x .> y_axis, :]
+# two_df = two_df[two_df.y .> x_axis, :]
 
 # two_df = filter( x -> x[:x] > y_axis && x[:y] > x_axis, one_df)
 
@@ -89,52 +94,84 @@ two_df = leftjoin(two_df, y_mean_std, on = :x)
 two_df = filter(x -> !isnan(x[:y_std]) && x[:y] > x[:y_mean] - x[:y_std]
 && x[:y] < x[:y_mean] + x[:y_std], two_df)
 
-two_df = combine(groupby(two_df, :x), :y => min)
-two_df = rename!(two_df, :y_min => :y)
+two_df = combine(groupby(two_df, :x), :y => minimum)
+two_df = rename!(two_df, :y_minimum => :y)
 
 # plot(two_df[:,:x], two_df[:,:y], seriestype = :scatter)
 
 # plot(two_df[:,:x], two_df[:,:y], seriestype = :scatter)
 # CSV.write("two_df.csv", two_df)
 # CSV.write("one_df_y_axis.csv", filter(:x => ==(maximum(y_axis)), one_df))
-y_ax_df = filter(:x => ==(maximum(y_axis)), one_df)
-y_lag = y_ax_df[2:end,:y]
-push!(y_lag, 0)
-y_ax_df = hcat(y_ax_df, y_lag)
-y_ax_df = rename(y_ax_df, :x1 => :y_lead)
-y_ax_df[!,:y_diff] = y_ax_df[:,:y_lead] - y_ax_df[:,:y]
-y_diff_mode = mode(filter(:y_diff => >(1), y_ax_df)[:,:y_diff])
-y_ax_df = filter(:y_diff => ==(y_diff_mode), y_ax_df)
-y_lag = y_ax_df[2:end,:y]
-push!(y_lag,0)
-y_ax_df = hcat(y_ax_df, y_lag)
-y_ax_df = rename(y_ax_df, :x1 => :y_lead_2)
-y_ax_df[!,:y_diff_2] = y_ax_df[:,:y_lead_2] - y_ax_df[:,:y]
-y_pixel_diff = mode(y_ax_df[:,:y_diff_2])
+
+# y_ax_df = filter(:x => ==(maximum(y_axis)-1), one_df)
+# y_lag = y_ax_df[2:end,:y]
+# push!(y_lag, 0)
+# y_ax_df = hcat(y_ax_df, y_lag)
+# y_ax_df = rename(y_ax_df, :x1 => :y_lead)
+# y_ax_df[!,:y_diff] = y_ax_df[:,:y_lead] - y_ax_df[:,:y]
+# y_diff_mode = mode(filter(:y_diff => >(1), y_ax_df)[:,:y_diff])
+# y_ax_df = filter(:y_diff => ==(y_diff_mode), y_ax_df)
+# y_lag = y_ax_df[2:end,:y]
+# push!(y_lag,0)
+# y_ax_df = hcat(y_ax_df, y_lag)
+# y_ax_df = rename(y_ax_df, :x1 => :y_lead_2)
+# y_ax_df[!,:y_diff_2] = y_ax_df[:,:y_lead_2] - y_ax_df[:,:y]
+# y_pixel_diff = mode(y_ax_df[:,:y_diff_2])
 
 
-x_ax_df = filter(:y => ==(maximum(x_axis)), one_df)
-x_lag = x_ax_df[2:end,:x]
-push!(x_lag, 0)
-x_ax_df = hcat(x_ax_df, x_lag)
-x_ax_df = rename(x_ax_df, :x1 => :x_lead)
-x_ax_df[!,:x_diff] = x_ax_df[:,:x_lead] - x_ax_df[:,:x]
-x_diff_mode = mode(filter(:x_diff => >(1), x_ax_df)[:,:x_diff])
-x_ax_df = filter(:x_diff => ==(x_diff_mode), x_ax_df)
-x_lag = x_ax_df[2:end,:x]
-push!(x_lag,0)
-x_ax_df = hcat(x_ax_df, x_lag)
-x_ax_df = rename(x_ax_df, :x1 => :x_lead_2)
-x_ax_df[!,:x_diff_2] = x_ax_df[:,:x_lead_2] - x_ax_df[:,:x]
-x_pixel_diff = mode(x_ax_df[:,:x_diff_2])
+# x_ax_df = filter(:y => ==(maximum(x_axis)-1), one_df)
+# x_lag = x_ax_df[2:end,:x]
+# push!(x_lag, 0)
+# x_ax_df = hcat(x_ax_df, x_lag)
+# x_ax_df = rename(x_ax_df, :x1 => :x_lead)
+# x_ax_df[!,:x_diff] = x_ax_df[:,:x_lead] - x_ax_df[:,:x]
+# x_diff_mode = mode(filter(:x_diff => >(1), x_ax_df)[:,:x_diff])
+# x_ax_df = filter(:x_diff => ==(x_diff_mode), x_ax_df)
+# x_lag = x_ax_df[2:end,:x]
+# push!(x_lag,0)
+# x_ax_df = hcat(x_ax_df, x_lag)
+# x_ax_df = rename(x_ax_df, :x1 => :x_lead_2)
+# x_ax_df[!,:x_diff_2] = x_ax_df[:,:x_lead_2] - x_ax_df[:,:x]
+# x_pixel_diff = mode(x_ax_df[:,:x_diff_2])
 
-chart_x = 50
-chart_y = 100
 
-x = (two_df[:,:x] .- x_axis) .* (chart_x/x_pixel_diff)
-y = (two_df[:,:y] .- y_axis) .* (chart_y/y_pixel_diff)
+
+# maximum(one_df[:,:x])
+# maximum(two_df[:,:x])
+# 1440-1435
+# maximum(one_df[:,:y])
+# maximum(two_df[:,:y])
+# 525-479
+# x = (two_df[:,:x] .- maximum(y_axis)) .* (chart_x/x_pixel_diff)
+# y = (two_df[:,:y] .- maximum(x_axis)) .* (chart_y/y_pixel_diff)
+
+# # x = (two_df[:,:x] .* (chart_x/x_pixel_diff)) .- (maximum(x_axis))
+# # y = (two_df[:,:y] .* (chart_y/y_pixel_diff)) .- (maximum(y_axis) + 46)
+x_start = 0
+y_start = 900
+x_end = 332
+y_end = 3
+
+
+#=
+472 -> 900
+3 -> 56
+469 -> 844
+
+39 -> 0
+1435 -> 332
+1396 -> 332
+
+=#
+
+
+x_factor = (x_end - x_start)/(last(two_df[:,:x]) - first(two_df[:,:x]))
+y_factor = (y_end - y_start)/(last(two_df[:,:y]) - first(two_df[:,:y]))
+
+x = ((two_df[:,:x]) .- minimum(two_df[:,:x])) .* (x_factor)
+y = ((two_df[:,:y]) .- minimum(two_df[:,:y])) .* (y_factor)
 
 plot(x, y, seriestype = :scatter)
 
 res_df = DataFrame(x = x,y = y)
-CSV.write("res.csv", res_df)
+CSV.write("res4.csv", res_df)
