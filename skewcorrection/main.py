4fbcef656313f31151f04325c733e12465140c2c
Madhav Kandukuri <Madhav.Kandukuri@ibm.com>
2022-08-29 19:00:19 +0530

    removed loop

--- skewcorrection/main.py
+++ skewcorrection/main.py
@@ -12,35 +12,35 @@ def find_score(arr, angle):
     hist = np.sum(data, axis=1)
     score = np.sum((hist[1:] - hist[:-1]) ** 2)
     return hist, score
-start = time.time()
-for i in range(100):
-    input_file = "./skewcorrection/original.png"
+# start = time.time()
+# for i in range(100):
+input_file = "./skewcorrection/original.png"
 
-    img = im.open(input_file)
+img = im.open(input_file)
 
-    # convert to binary
-    wd, ht = img.size
-    pix = np.array(img.convert('1').getdata(), np.uint8)
-    bin_img = 1 - (pix.reshape((ht, wd)) / 255.0)
-    plt.imshow(bin_img, cmap='gray')
-    plt.savefig('./skewcorrection/binary_py.png')
+# convert to binary
+wd, ht = img.size
+pix = np.array(img.convert('1').getdata(), np.uint8)
+bin_img = 1 - (pix.reshape((ht, wd)) / 255.0)
+plt.imshow(bin_img, cmap='gray')
+plt.savefig('./skewcorrection/binary_py.png')
 
-    delta = 1
-    limit = 5
-    angles = np.arange(-limit, limit+delta, delta)
-    scores = []
-    for angle in angles:
-        hist, score = find_score(bin_img, angle)
-        scores.append(score)
+delta = 1
+limit = 5
+angles = np.arange(-limit, limit+delta, delta)
+scores = []
+for angle in angles:
+    hist, score = find_score(bin_img, angle)
+    scores.append(score)
 
-    best_score = max(scores)
-    best_angle = angles[scores.index(best_score)]
-    print('Best angle: {}'.format(best_angle))
+best_score = max(scores)
+best_angle = angles[scores.index(best_score)]
+print('Best angle: {}'.format(best_angle))
 
-    # correct skew
-    data = inter.rotate(bin_img, best_angle, reshape=False, order=0)
-    img = im.fromarray((255 * data).astype("uint8")).convert("RGB")
-    img.save('./skewcorrection/skew_corrected_py.png')
-end = time.time()
+# correct skew
+data = inter.rotate(bin_img, best_angle, reshape=False, order=0)
+img = im.fromarray((255 * data).astype("uint8")).convert("RGB")
+img.save('./skewcorrection/skew_corrected_py.png')
+# end = time.time()
 
-print('Time taken: {}'.format(end-start))
+# print('Time taken: {}'.format(end-start))
