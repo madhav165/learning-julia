8a2d93a058d273534cea014512bd09f953fad196
Madhav Kandukuri <Madhav.Kandukuri@ibm.com>
2022-10-02 20:38:33 +0530

    Revert "fixed import"
    This reverts commit f9a0c2b4ddc5b4c5ba587e99d926439d65373536.

--- SocEstimator/bootstrap.jl
+++ SocEstimator/bootstrap.jl
@@ -1,7 +1,5 @@
 (pwd() != @__DIR__) && cd(@__DIR__) # allow starting app from bin/ dir
 
-include("./src/SocEstimator.jl")
-
-using .SocEstimator
+using SocEstimator
 const UserApp = SocEstimator
 SocEstimator.main()
