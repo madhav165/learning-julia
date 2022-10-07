126d7607c56d876e0b48c21aec5f050355e5cb18
Madhav Kandukuri <Madhav.Kandukuri@ibm.com>
2022-10-07 10:26:31 +0530

    separate modules for GMaps and Plugshare

---
+++ SocBot_Plugshare/psql_backend.jl
@@ -0,0 +1,140 @@
+module Backend
+
+using LibPQ
+using Tables
+using Dates
+
+using DotEnv
+DotEnv.config()
+
+# export conn, init_db, get_conn, update_contact_list, get_key, set_key, get_state, set_state
+
+postgres_user=ENV["POSTGRES_USER"]
+postgres_password=ENV["POSTGRES_PASSWORD"]
+postgres_db=ENV["POSTGRES_DB"]
+user_table=ENV["USER_TABLE"]
+trip_table=ENV["TRIP_TABLE"]
+car_table=ENV["CAR_TABLE"]
+key=ENV["TELEGRAM_KEY"]
+
+conn = -1
+
+function update_contact_list(message)
+    id = message["message"]["from"]["id"]
+    res = execute(conn, "SELECT * from $user_table WHERE id=$id;")
+    if length(columntable(res)[1]) == 0
+       res = execute(conn, "INSERT INTO $user_table VALUES ($id,0);")
+    end
+end
+
+function update_trip_list(message)
+    id = message["message"]["from"]["id"]
+    update_id = message["update_id"]
+    id_contac = parse(Int, string(id) * string(update_id))
+    datetime = Dates.unix2datetime(message["message"]["date"])
+    res = execute(conn, "SELECT * from $trip_table WHERE id=$id_contac;")
+    if length(columntable(res)[1]) == 0
+       res = execute(conn, "INSERT INTO $trip_table VALUES ($id_contac, $id, '$datetime');")
+    end
+end
+
+function send_message(params)
+    req = HTTP.request("POST",string(url,"bot",key,"/sendMessage"),["Content-Type" => "application/json"],JSON.json(params))
+end
+
+function get_key(table, id, key)
+    if table == "user"
+        res = execute(conn, "SELECT * from $user_table WHERE id=$id;")
+        data = Tables.columntable(res)
+        keys = Dict("id"=>data[1][1], "state"=>data[2][1], "car"=>data[3][1], "current_trip_id"=>data[4][1])
+        return keys[key]
+    elseif table == "trip"
+        res = execute(conn, "SELECT * from $trip_table WHERE id=$id;")
+        data = Tables.columntable(res)
+        keys = Dict("id"=>data[1][1], "user_id"=>data[2][1], "datetime"=>data[3][1], 
+        "origin"=>data[4][1], "destination"=>data[5][1])
+        return keys[key]
+    elseif table == "car"
+        res = execute(conn, "SELECT * from $car_table;")
+        data = Tables.columntable(res)
+        keys = Dict("id"=>data[1][1], "car_name"=>data[2][1], "battery_capacity_wh"=>data[3][1], 
+        "usable_capacity_share"=>data[4][1])
+        return keys[key]
+    end 
+end
+
+function set_key(table, id, key, value)
+    if table == "user"
+        res = execute(conn, "UPDATE $user_table SET $(key)='$value' WHERE id=$id;")
+    elseif table == "trip"
+        res = execute(conn, "UPDATE $trip_table SET $(key)='$value' WHERE id=$id;")
+    end
+end
+
+function get_state(table, id)
+    return get_key(table, id,"state")
+end
+
+function set_state(table, id, state)
+    set_key(table, id,"state", state)
+end
+
+function get_car(table, id)
+    return get_key(table, id, "car")
+end
+
+function init_db()
+    # conn = LibPQ.Connection("dbname=$postgres_db user=$postgres_user password=$postgres_password host=database")
+    conn = LibPQ.Connection("dbname=$postgres_db user=$postgres_user password=$postgres_password")
+
+    result = execute(conn, """
+        CREATE TABLE IF NOT EXISTS $user_table (
+            id integer PRIMARY KEY,
+            state  integer,
+            car varchar(100),
+            current_trip_id bigint
+        );
+    """)
+
+    result = execute(conn, """
+        CREATE TABLE IF NOT EXISTS $trip_table (
+            id bigint PRIMARY KEY,
+            user_id integer,
+            datetime timestamp,
+            origin varchar(200),
+            destination varchar(200)
+        );
+    """)
+
+    result = execute(conn, """
+    DROP TABLE IF EXISTS $car_table;    
+    CREATE TABLE IF NOT EXISTS $car_table (
+            id integer PRIMARY KEY,
+            car_name varchar(50),
+            battery_capacity_wh integer,
+            usable_capacity_share float
+        );
+        INSERT INTO $car_table VALUES (1, 'Nexon EV Prime', 30200, 0.95);
+        INSERT INTO $car_table VALUES (2, 'Nexon EV Max', 40500, 0.95);
+        INSERT INTO $car_table VALUES (3, 'Tigor EV', 26000, 0.95);
+        INSERT INTO $car_table VALUES (4, 'ZS EV 2021', 43000, 0.95);
+        INSERT INTO $car_table VALUES (5, 'ZS EV 2022', 50300, 0.95);
+        INSERT INTO $car_table VALUES (6, 'Kona EV', 40500, 0.95);
+        INSERT INTO $car_table VALUES (7, 'Tiago EV MR', 19000, 0.95);
+        INSERT INTO $car_table VALUES (8, 'Tiago EV LR', 24000, 0.95);
+    """)
+
+    close(conn)
+end
+
+
+function get_conn()
+    global conn
+    if conn == -1
+        # conn = LibPQ.Connection("dbname=$postgres_db user=$postgres_user password=$postgres_password host=database")
+        conn = LibPQ.Connection("dbname=$postgres_db user=$postgres_user password=$postgres_password")
+    end
+    return conn
+end
+
+end
---
+++ SocBot_Plugshare/soc_bot.jl
@@ -0,0 +1,172 @@
+using Printf
+using JSON
+using Dates
+
+using DotEnv
+DotEnv.config()
+
+#include("white_backend.jl")
+include("psql_backend.jl")
+using Main.Backend
+
+include("telegram.jl")
+using Main.Telegram
+
+function send_welcome(contact)
+    id = contact["id"]
+    name = contact["first_name"]
+
+    params = Dict("chat_id"=>id, "text"=>string("Hi there, ", name, "!
+    
+I can help estimate the charging needs for your trip so that you can be free of range anxiety.
+   
+Type /plantrip to get started."))
+
+    Telegram.send_message(params)
+end
+
+function ask_car(contact)
+    id = contact["id"]
+    cars = Backend.get_key("car", 1, "car_name")
+    @info cars
+    buttons = [Dict("text"=>"Nexon EV Prime"), Dict("text"=>"Nexon EV Max")]
+    reply_markup = Dict("keyboard"=>[buttons], "one_time_keyboard"=>true)
+    params = Dict("chat_id"=>id, "text"=>"What car do you drive?", "reply_markup"=>reply_markup)
+    Telegram.send_message(params)
+end
+
+function ask_origin(message, state)
+    id = message["from"]["id"]
+    if state == 2
+        car = message["text"]
+
+        if !(car  in ["Nexon EV Prime" "Nexon EV Max"])
+            car = "Nexon EV Prime"
+        end
+
+        params = Dict("chat_id"=>id, "text"=>string("Saving your car as ", car))
+        Telegram.send_message(params)
+
+        params = Dict("chat_id"=>id, "text"=>string("Where are you starting your journey?"))
+        Telegram.send_message(params)
+        return car
+    end
+    params = Dict("chat_id"=>id, "text"=>string("Where are you starting your journey?"))
+    Telegram.send_message(params)
+end
+
+function ask_destination(message)
+    id = message["from"]["id"]
+    origin = message["text"]
+
+    params = Dict("chat_id"=>id, "text"=>string("Where are you headed for?"))
+    Telegram.send_message(params)
+    return origin
+end
+
+function finalize_trip(message)
+    id = message["from"]["id"]
+    destination = message["text"]
+    datetime = Dates.unix2datetime(message["date"])
+    return datetime, destination
+end
+
+function summarize_trip(message)
+    id = message["from"]["id"]
+    car = Backend.get_key("user", id, "car")
+    current_trip_id = Backend.get_key("user", id, "current_trip_id")
+    origin = Backend.get_key("trip", current_trip_id, "origin")
+    destination = Backend.get_key("trip", current_trip_id, "destination")
+    
+    params = Dict("chat_id"=>id, "text"=>string("Wish you a happy journey from $origin 
+to $destination in your $car."))
+    Telegram.send_message(params)
+end
+
+function parse_message(message)
+    id = message["message"]["from"]["id"]
+    update_id = message["update_id"]
+    Backend.update_contact_list(message)
+    state = Backend.get_state("user", id)
+
+    if (state == 0) | (message["message"]["text"]=="/start")
+        @info "welcoming user"
+        send_welcome(message["message"]["from"])
+        Backend.set_state("user", id, 1)
+    elseif message["message"]["text"]=="/plantrip"
+        Backend.update_trip_list(message)
+        Backend.set_key("user", id, "current_trip_id", parse(Int, string(id) * string(update_id)))
+        if state <= 1
+            car = Backend.get_car("user", id)
+            if car === missing
+                @info "requesting car info"
+                ask_car(message["message"]["from"])
+                Backend.set_state("user", id, 2)
+            else
+                @info "car info found and asking for origin"
+                ask_origin(message["message"], state)
+                Backend.set_state("user", id, 4)
+            end
+        else
+            @info "asking for origin"
+            ask_origin(message["message"], state)
+            Backend.set_state("user", id, 4)
+        end
+    elseif state == 2
+        @info "saving car info and asking for origin"
+        car = ask_origin(message["message"], state)
+        Backend.set_state("user", id, 4)
+        Backend.set_key("user", id,"car",car)
+    elseif state == 3
+        @info "asking for origin"
+        ask_origin(message["message"], state)
+        Backend.set_state("user", id, 4)
+    elseif state == 4
+        @info "asking for destination"
+        origin = ask_destination(message["message"])
+        Backend.set_state("user", id, 5)
+        current_trip_id = Backend.get_key("user", id, "current_trip_id")
+        Backend.set_key("trip", current_trip_id, "origin", origin)
+    elseif state == 5
+        @info "sharing trip summary"
+        datetime, destination = finalize_trip(message["message"])
+        current_trip_id = Backend.get_key("user", id, "current_trip_id")
+        Backend.set_key("trip", current_trip_id, "datetime", datetime)
+        Backend.set_key("trip", current_trip_id, "destination", destination)
+        summarize_trip(message["message"])
+    end
+end
+
+function run()
+    offset = -1
+
+    @info "app is ready!"
+    while true
+        if offset >= 0
+            params = Dict("offset"=>offset)
+        else
+            params = Dict()
+        end
+        req = Telegram.get_updates(params)
+        body = String(req.body)
+        result = JSON.Parser.parse(body)
+
+        if length(result["result"]) < 1
+            sleep(0.02)
+            continue
+        end
+        offset = result["result"][end]["update_id"] + 1
+        for message in result["result"]
+           parse_message(message)
+        end
+        sleep(0.1)
+    end
+end
+
+
+Backend.init_db()
+conn = Backend.get_conn()
+
+run()
+
+@sync close(conn)
