1f15f145aab71564f52ddaddb61c9c01d1ea0c41
Madhav Kandukuri <madhav165@gmail.com>
2022-10-17 11:32:36 +0530

    corrected calc

--- SocBot_ORS/ors.jl
+++ SocBot_ORS/ors.jl
@@ -68,7 +68,14 @@ function get_directions(origin_coordinates::Vector{Float64}, destination_coordin
     df_wh[!, :distance_km] = round.(df_wh[!, :distance_m] ./ 1000; digits=1)
     df_wh[!, :elevation_diff_cum] = cumsum(df_wh[!, :elevation_diff_m])
     df_wh.slope = df_wh[!,"elevation_diff_cum"] ./ df_wh[!,"distance_m"]
-    df_wh.wh_per_km = 130 .+ (df_wh[!,"slope"] .* 5000)
+    # df_wh.wh_per_km = 130 .+ (df_wh[!,"slope"] .< 0 ? df_wh[!,"slope"] .* 10000 : df_wh[!,"slope"].* 2500)
+    # df_wh.wh_per_km = 127 .+ ifelse.(df_wh[!,"slope"] .< 0, df_wh[!,"slope"] .* 3000, df_wh[!,"slope"] .* 10000)
+    # df_wh.wh_per_km = 130 .+ ifelse.(df_wh[!,"slope"] .< 0, (exp.(df_wh[!, "slope"]) .- 1) .* 10000, (exp.(df_wh[!, "slope"]) .- 1) .* 20000)
+    # df_wh.wh_per_km = 122 .+ (df_wh[!, "slope"] .* 50)
+    df_wh.wh_per_km = ifelse.(df_wh[!,"slope"] .< 0, 25, 127 .+ df_wh[!,"slope"] .* 4355)
+    # df_wh = @transform(df_wh, :wh_per_km = :slope < 0 ? 130 + :slope * 10000 : 130 + :slope * 5000)
+
+
     df_wh.wh_used = df_wh[!, "wh_per_km"] .* df_wh[!, "dist_per_wp_m"] ./ 1000
     df_wh[isnan.(df_wh.wh_used), :wh_used] .= 0
     df_wh[!, :total_wh_used] = cumsum(df_wh[!, :wh_used])
